93730c53f92ad441a86cf77c004f0bfd67db7207
Rio Simpson <rsimpson@baretzbrunelle.com>
2025-07-09 08:28:32 -0400

    container and controller for modules

---
+++ src/components/widgets/attorney_widget.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+
+// modules
+import AttorneyModule from "../modules/attorney_mod.jsx";
+
+// css
+import "./attorney_widget.css";
+
+const AttorneyWidget = () => {
+    const [attorneys, setAttorneys] = useState([]);
+
+    useEffect(() => {
+        const fetchAttorneys = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/dashboard/get-attorneys`);
+                const data = await response.json();
+
+                // Process images
+                const processedData = data.map(attorney => {
+                    const nameKey = attorney.name.replace(/\s+/g, "_") + "_headshot";
+                    
+                    let imageURL;
+
+                    // Check localStorage for existing image
+                    const cachedImage = localStorage.getItem(nameKey);
+                    if (cachedImage) {
+                        imageURL = cachedImage;
+                    } else {
+                        if (attorney.image) {
+                            imageURL = `data:image/jpeg;base64,${attorney.image}`;
+                            localStorage.setItem(nameKey, imageURL);
+                        } else {
+                            imageURL = ""; // fallback if no image
+                        }
+                    }
+
+                    // console.log(attorney.tags);
+
+                    return {
+                        ...attorney,
+                        image: imageURL
+                    };
+                });
+
+                setAttorneys(processedData);
+            } catch (err) {
+                console.error("Error fetching attorneys:", err);
+            }
+        };
+
+        fetchAttorneys();
+    }, []);
+
+    return (
+        <div className="attorney-widget-main-container">
+            {attorneys.map((attorney, index) => (
+                <AttorneyModule
+                    key={index}
+                    name={attorney.name}
+                    image={attorney.image}
+                    linkedinURL={attorney.linkedin}
+                    websiteURL={attorney.website}
+                    jdYear={attorney.grad_year}
+                    location={attorney.location}
+                    currentWorkplace={attorney.current_workplace}
+                    phoneNumber={attorney.phone_number}
+                    email={attorney.email}
+                    summary={attorney.summary}
+                    tags={attorney.tags}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default AttorneyWidget;

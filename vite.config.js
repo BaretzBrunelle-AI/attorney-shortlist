8855be2fef7d8d28f2fb1e55d99bd2cc35a59615
Rio Simpson <rsimpson@baretzbrunelle.com>
2025-09-22 22:54:52 +0200

    simplified vite app config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.ts/js
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    proxy: {
-      "/admin": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      "/dashboard": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      "/healthz": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-  build: { outDir: "dist", sourcemap: false },
-});
+})
